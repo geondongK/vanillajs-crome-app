fcac864c5343f6279938c22e70859a88d0830524
geondongK <ehd563@naver.com>
2021-01-14 14:02:39 +0900

    user localStorage 저장

--- greeting.js
+++ greeting.js
@@ -5,17 +5,33 @@ const form = document.querySelector('.js-form'),
 const USER_STOR = "currentUser",
     SHOWING_CN = "showing";
 
+function savaName(text) {
+    localStorage.setItem(USER_STOR, text);
+}
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    savaName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener('submit', handleSubmit)
+}
+
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`
+    greeting.innerText = `Hello ${text}`;
 }
 
 //LocalStorage에서 데이터 가져오기.
 function loadName() {
     const currentUser = localStorage.getItem(USER_STOR);
     if (currentUser === null) {
-        // he is not
+        askForName();
     } else {
         paintGreeting(currentUser);
     }
